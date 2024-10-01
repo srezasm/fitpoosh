6d2c4e14b1fc830dfa9b2baae841e39783be36e7
srezasm <github-action@actions-template-sync.noreply.github.com>
2024-10-01 01:20:57 +0000

    chore(template): merge template changes :up:
    Signed-off-by: srezasm <github-action@actions-template-sync.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,20 +4,8 @@ import "@/styles/date-picker.css";
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import localFont from "next/font/local";
-import { useEffect } from "react";
-
-const sahelFont = localFont({
-  src: "../../public/fonts/Sahel-VF.woff2",
-  variable: "--sahel",
-  preload: true,
-  display: "swap",
-});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    document.body.classList.add(sahelFont.variable);
-  }, []);
   return (
     <PlasmicRootProvider Head={Head}>
       <Component {...pageProps} />
